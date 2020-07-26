1ceae6bc12e152aaa9a70691146ca8989dbbe546
ElectNewt <ivanxo69@gmail.com>
2020-07-26 22:29:32 +0100

    add ejemplo docker.

---
+++ src/BackEnd/src/WebPersonal.BackEnd.API/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using WebPersonal.Shared.Dto;
+
+namespace WebPersonal.BackEnd.API.Controllers
+{
+  
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("ejemplo-docker")]
+        public string Get()
+        {
+            Console.WriteLine("docker");
+           return "true";
+        }
+    }
+}
