0dd55e2cf4a2389bce037a351afe7e8f6e273453
ElectNewt <ivanxo69@gmail.com>
2020-09-16 18:04:41 +0100

    fix bug on gitignore

---
+++ Tools/ServerMySql/Database/Version01.sql
@@ -0,0 +1,100 @@
+
+CREATE DATABASE IF NOT EXISTS `webpersonal`;
+USE `webpersonal`;
+CREATE USER 'webpersonaluser' IDENTIFIED BY 'webpersonalpass';
+GRANT ALL PRIVILEGES ON * . * TO 'webpersonaluser';
+
+
+
+CREATE TABLE IF NOT EXISTS `academicproject` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `educationid` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `details` varchar(5000) NOT NULL,
+  `environment` varchar(500) NOT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `education` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `startdate` date NOT NULL,
+  `enddate` date DEFAULT NULL,
+  `coursename` varchar(250) NOT NULL,
+  `collegename` varchar(250) DEFAULT NULL,
+  `city` varchar(250) DEFAULT NULL,
+  `country` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+CREATE TABLE IF NOT EXISTS `interest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `personalprofile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `website` varchar(50) DEFAULT NULL,
+  `github` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `personalproject` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `details` varchar(5000) NOT NULL,
+  `environment` varchar(500) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE IF NOT EXISTS `skill` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `punctuation` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `userid` (
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(50) NOT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `UserName` (`UserName`)
+);
+
+CREATE TABLE IF NOT EXISTS `workexperience` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `position` varchar(150) NOT NULL,
+  `companyname` varchar(150) NOT NULL,
+  `city` varchar(150) NOT NULL,
+  `country` varchar(150) NOT NULL,
+  `startdate` date DEFAULT NULL,
+  `enddate` date DEFAULT NULL,
+  `environment` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `workproject` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `workid` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `details` varchar(5000) NOT NULL,
+  `date` date DEFAULT NULL,
+  `environment` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
