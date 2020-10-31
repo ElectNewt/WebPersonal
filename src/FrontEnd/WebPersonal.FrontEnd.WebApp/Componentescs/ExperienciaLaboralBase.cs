92e15d956a6ca1ab8c5383efd4279a05d920823b
ElectNewt <ivanxo69@gmail.com>
2020-10-31 13:48:50 +0000

    add blazor components

---
+++ src/FrontEnd/WebPersonal.FrontEnd.WebApp/Componentescs/ExperienciaLaboralBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPersonal.FrontEnd.WebApp.Componentescs
+{
+    public class ExperienciaLaboralBase : ComponentBase
+    {
+        public string Titulo => "Experiencia Laboral";
+    }
+}
