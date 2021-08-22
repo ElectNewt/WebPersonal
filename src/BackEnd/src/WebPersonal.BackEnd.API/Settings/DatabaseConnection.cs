56472cff7d7549795711a089c929bbdc370cde06
ElectNewt <ivanxo69@gmail.com>
2021-08-22 19:41:50 +0100

    implementar iconfiguration

---
+++ src/BackEnd/src/WebPersonal.BackEnd.API/Settings/DatabaseConnection.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace WebPersonal.BackEnd.API.Settings
+{
+    public static class Database
+    {
+        public static string BuildConnectionString(IConfiguration config)
+        {
+            DatabaseSettings dbSettings = new DatabaseSettings();
+            config.Bind("database", dbSettings);
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Server={dbSettings.Server};");
+            sb.Append($"Port={dbSettings.Port};");
+            sb.Append($"Database={dbSettings.DatabaseName};");
+            sb.Append($"Uid={dbSettings.User};");
+            sb.Append($"password={dbSettings.Password};");
+
+            if (dbSettings.AllowUserVairables == true)
+            {
+                sb.Append("Allow User Variables=True;");
+            }
+
+            return sb.ToString();
+        }
+
+       
+
+        public class DatabaseSettings
+        {
+            public string Server { get; set;  }
+            public int Port { get; set; }
+            public string DatabaseName { get; set; }
+            public string User { get; set; }
+            public string Password { get; set; }
+            public bool AllowUserVairables { get; set; }
+        }
+    }
+}
--- src/BackEnd/src/WebPersonal.BackEnd.API/Startup.cs
+++ src/BackEnd/src/WebPersonal.BackEnd.API/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MySql.Data.MySqlClient;
@@ -9,6 +10,7 @@ using System.Data.Common;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using WebPersonal.BackEnd.API.Settings;
 using WebPersonal.BackEnd.Model.Repositories;
 using WebPersonal.BackEnd.Service.PerfilPersonal;
 using WebPersonal.BackEnd.ServiceDependencies.Services.PerfilPersonal;
@@ -18,6 +20,13 @@ namespace WebPersonal.BackEnd.API
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+       
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,12 +35,13 @@ namespace WebPersonal.BackEnd.API
             services.AddControllers();
             services.AddDataProtection();
 
-
             //Todo:Move this to their respecives projects 
             //Temporal connection until explained different environments.
+
+
             services
                 .AddSingleton<IHttpContextAccessor, HttpContextAccessor>()
-                .AddScoped<DbConnection>(x => new MySqlConnection("Server=127.0.0.1;Port=3306;Database=webpersonal;Uid=webpersonaluser;password=webpersonalpass;Allow User Variables=True"))
+                .AddScoped<DbConnection>(x => new MySqlConnection(Database.BuildConnectionString(Configuration)))
                 .AddScoped<TransactionalWrapper>()
                 .AddScoped<PersonalProfile>()
                 .AddScoped<PutPersonalProfile>()
