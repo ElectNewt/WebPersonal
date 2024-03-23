5eeac47e4b9bee7e901fc783e25ae1ce8b3be753
Netmentor <ivanxo69@gmail.com>
2024-03-23 22:17:03 +0000

    problemdetails example

--- src/BackEnd/src/WebPersonal.BackEnd.API/Controllers/PerfilPersonalController.cs
+++ src/BackEnd/src/WebPersonal.BackEnd.API/Controllers/PerfilPersonalController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using ROP;
 using ROP.APIExtensions;
@@ -16,7 +18,8 @@ namespace WebPersonal.BackEnd.API.Controllers
         private readonly PutPersonalProfile _putPersonalProfile;
         private readonly PostPersonalProfile _postPersonalProfile;
 
-        public PerfilPersonalController(PersonalProfile getPersonalProfile, PutPersonalProfile putPersonalProfile, PostPersonalProfile postPersonalProfile)
+        public PerfilPersonalController(PersonalProfile getPersonalProfile, PutPersonalProfile putPersonalProfile,
+            PostPersonalProfile postPersonalProfile)
         {
             _getPersonalProfile = getPersonalProfile;
             _putPersonalProfile = putPersonalProfile;
@@ -28,24 +31,52 @@ namespace WebPersonal.BackEnd.API.Controllers
         {
             return await GetProfile(userName).ToActionResult();
         }
-       
-        private async Task<Result<PersonalProfileDto>> GetProfile(string userName) {
+
+        private async Task<Result<PersonalProfileDto>> GetProfile(string userName)
+        {
             return await _getPersonalProfile.GetPersonalProfileDto(userName);
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(PersonalProfileDto profileDto)
         {
+            ProblemDetails problemDetails = new ProblemDetails()
+            {
+                Title = "Error en la validación de los datos",
+                Detail = "Los parámetros no son correctos",
+                Status = 400,
+                Type = "https://website.net/code-error-1",
+            };
+
+            List<(string, string)> errors = new();
+            //Validar email
+            if (!IsValidEmail(profileDto.Email))
+            {
+                errors.Add(("Email", "El email no es válido"));
+            }
+            
+            //validar numero de telefono es un numero
+            if (!long.TryParse(profileDto.Phone, out _))
+            {
+                errors.Add(("Phone", "El número de teléfono no es válido"));
+            }
+
+            if (errors.Any())
+            {
+                problemDetails.Extensions.Add("Errors", errors.ToDictionary());
+                return new ObjectResult(problemDetails);
+            }
+
             return await _postPersonalProfile.Create(profileDto)
                 .Bind(x => GetProfile(x.UserName))
                 .ToActionResult();
         }
-        
+
         [HttpPost("returnonlyid")]
         public async Task<Result<int?>> PostId(PersonalProfileDto profileDto)
         {
             return await _postPersonalProfile.Create(profileDto)
-                .Map(x=>x.UserId);
+                .Map(x => x.UserId);
         }
 
         [HttpPut]
@@ -54,6 +85,18 @@ namespace WebPersonal.BackEnd.API.Controllers
             return await _putPersonalProfile.Create(profileDto)
                 .Bind(x => GetProfile(x.UserName));
         }
-    }
 
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
 }
